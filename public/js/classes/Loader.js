1703824a38725153892d6d79e555f61e616c1782
Peter Wagner <peter.wagner1986@gmail.com>
2021-06-01 16:28:37 +0200

    added new models

--- public/js/classes/Loader.js
+++ public/js/classes/Loader.js
@@ -1,6 +1,6 @@
 export default class Loader{
 
-    constructor(loader, scene, model, size, x, y, z){
+    constructor(loader, scene, model, size, x, y, z, rotation = 0){
         this.loader = loader;
         this.scene = scene;
         this.model = model;
@@ -8,6 +8,7 @@ export default class Loader{
         this.x = x;
         this.y = y;
         this.z = z;
+        this.rotation = rotation
         this.loadModel();
     };
 
@@ -16,14 +17,15 @@ export default class Loader{
             this.scene.add(gltf.scene);
             gltf.scene.scale.set(this.size, this.size, this.size);
             gltf.scene.position.set(this.x, this.y, this.z);
+            gltf.scene.rotation.y = this.rotation
             gltf.castShadow = true;
             gltf.receiveShadow = true;
         }, 
             (xhr) => {
-                console.log( `${( xhr.loaded / xhr.total * 100 )}% loaded` )
+                console.log( `${Math.floor( xhr.loaded / xhr.total * 100 )}% loaded` )
                 if(xhr.loaded / xhr.total === 1){
-                    console.log('completed');
-                }
+                    console.log(`${this.model} completed`);
+                };
             },
             (err) => console.log(`An error happend. ${err}`)
         );
--- public/js/main.js
+++ public/js/main.js
@@ -54,26 +54,19 @@ const init = () => {
     const backwall = new Cube(20.67, 10, 0.67).create();
     backwall.position.set(-0.85, 4.36, -10.85);
     textureLoader.load('/textures/sandstone/sandstone_blocks_08_diff_1k.png',
-    (texture) => {
-        backwall.material = new THREE.MeshBasicMaterial({ map: texture });
-    },
+    (texture) => backwall.material = new THREE.MeshBasicMaterial({ map: texture }),
     (err) => console.error(`An error happend. ${err}`)
     );   
 
     // First Floor
     const firstFloor = new Cube(10.2, 0.67, 20).create();
     firstFloor.position.set(4.4, 4.6, -0.5);
-    textureLoader.load('/textures/floor/marble_01_spec_1k.png',
-    (texture) => {
-        firstFloor.material = new THREE.MeshBasicMaterial({ map: texture });
-    },
-    (err) => console.error(`An error happend. ${err}`)
-    );
 
     const firstFloor2 = new Cube(9.8, 0.67, 13.7).create();
     firstFloor2.position.set(-5.6, 4.6, -3.7);
     textureLoader.load('/textures/floor/marble_01_spec_1k.png',
     (texture) => {
+        firstFloor.material = new THREE.MeshBasicMaterial({ map: texture });
         firstFloor2.material = new THREE.MeshBasicMaterial({ map: texture });
     },
     (err) => console.error(`An error happend. ${err}`)
@@ -82,6 +75,15 @@ const init = () => {
     // Furniture loading
     const couch = new Loader(gltfLoader, scene, 'modern_couch', 0.025, -5, -0.01, -4.5);
     const stairs = new Loader(gltfLoader, scene, 'stairs', 0.037, -7.7, -0.01, 8.8);
+    const plant = new Loader(gltfLoader, scene, 'plant', 0.5, -9, 0, 8);
+    const coffeTable = new Loader(gltfLoader, scene, 'coffe_table', 0.02, -3, 0, -3);
+    const pottedPlant = new Loader(gltfLoader, scene, 'potted_plant', 0.05, -3, 0.85, -2.8);
+    const bed = new Loader(gltfLoader, scene, 'cubes_bed', 3, -7.5, 5.85, -7, -Math.PI / 2);
+    const cyberDesk = new Loader(gltfLoader, scene, 'cyber_desk', 0.6, 5, 4.6, -6.3);
+    const modernTable = new Loader(gltfLoader, scene, 'modern_table', 0.01, 5, 0, 5, Math.PI / 2);
+    const poolTable = new Loader(gltfLoader, scene, 'pool_table', 0.05, 5, 5.5, 0);
+    const bar = new Loader(gltfLoader, scene, 'bar_table', 0.07, 6, 4.7, 7);
+    const kitchen = new Loader(gltfLoader, scene, 'kitchen', 1.5, 4.5, 0, -9.7);
 
     // Adding to scene
     scene.add(
